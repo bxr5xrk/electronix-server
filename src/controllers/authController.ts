4ffab487863109a22c4facac11b94e94d4785558
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-03-19 20:20:15 +0200

    updated auth

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,9 +4,10 @@ import AuthService from '../service/authService';
 class AuthController {
   login = async (req: Request, res: Response): Promise<void> => {
     const { email, password } = req.body;
+
     try {
-      const token = await AuthService.login(email, password);
-      res.status(200).json({ token });
+      const data = await AuthService.login(email, password);
+      res.status(200).json({ ...data });
     } catch (error) {
       console.error(error);
       res.status(401).json({ message: 'Invalid credentials' });
@@ -15,9 +16,10 @@ class AuthController {
 
   register = async (req: Request, res: Response): Promise<void> => {
     const { name, email, password } = req.body;
+
     try {
-      await AuthService.register(name, email, password);
-      res.status(201).json({ message: 'User created successfully' });
+      const data = await AuthService.register(name, email, password);
+      res.status(201).json({ message: 'User created successfully', ...data });
     } catch (error) {
       console.error(error);
       if (error instanceof Error) {
--- src/service/authService.ts
+++ src/service/authService.ts
@@ -1,37 +1,47 @@
 import { compare, hash } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
-import { User } from '../db/models/user';
+import { Role, User } from '../db/models/user';
 import { query } from '../db';
 import { config } from '../config';
 
 class AuthService {
-  login = async (email: string, password: string): Promise<string> => {
+  login = async (
+    email: string,
+    password: string
+  ): Promise<{ token: string; name: string; email: string; role: Role }> => {
     const result = await query<User>('SELECT * FROM "user" WHERE email = $1', [
       email,
     ]);
+
     const user = result.rows[0];
+
     if (!user) {
       throw new Error('Invalid credentials');
     }
+
     const isMatch = await compare(password, user.password);
+
     if (!isMatch) {
       throw new Error('Invalid credentials');
     }
+
     const token = sign({ id: user.id, role: user.role }, config.JWT_SECRET);
-    return token;
+
+    return { token, name: user.name, email: user.email, role: user.role };
   };
 
   register = async (
     name: string,
     email: string,
     password: string
-  ): Promise<void> => {
-    const result = await query<User>('SELECT * FROM "user" WHERE email = $1', [
+  ): Promise<{ token: string; name: string; email: string; role: Role }> => {
+    const user = await query<User>('SELECT * FROM "user" WHERE email = $1', [
       email,
     ]);
 
-    const user = result.rows[0];
-    if (user) {
+    const findUser = user.rows[0];
+
+    if (findUser) {
       throw new Error('User already exists');
     }
 
@@ -41,6 +51,12 @@ class AuthService {
       'INSERT INTO "user" (name, email, password, role) VALUES ($1, $2, $3, $4)',
       [name, email, hashedPassword, 'client']
     );
+
+    // Generate a JSON Web Token (JWT) with the user ID and role
+    const token = sign({ email, role: 'client' }, config.JWT_SECRET);
+
+    // Return the JWT, along with the user's name, email, and role
+    return { token, name, email, role: 'client' };
   };
 }
 
